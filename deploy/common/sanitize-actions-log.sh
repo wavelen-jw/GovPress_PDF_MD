2f118016e3ae9f6d2656b2aba676cdbbb8230639
Cloud User <ubuntu@mois-ai-lab-03.novalocal>
2026-06-30 20:34:03 +0900

    Omit verbose probe bodies from deploy diagnostics

--- deploy/common/sanitize-actions-log.sh
+++ deploy/common/sanitize-actions-log.sh
@@ -5,4 +5,5 @@ sed -E \
   -e 's/(--token[= ]+)[^[:space:]]+/\1[REDACTED]/g' \
   -e 's/(--service-token-secret[= ]+)[^[:space:]]+/\1[REDACTED]/g' \
   -e 's/(CLOUDFLARE_TUNNEL_TOKEN=)[^[:space:]]+/\1[REDACTED]/g' \
+  -e 's/((public_)?(policy|health)_probe_body=)[^;[:cntrl:]]+/\1[OMITTED]/g' \
   -e 's#(https://)[^/@[:space:]]+@github\.com#\1[REDACTED]@github.com#g'
